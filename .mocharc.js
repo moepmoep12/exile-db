aec7aca9499b95f3639063c50e5c02e1b91af016
Andreas Schmidt <moepmoep12@gmail.com>
2022-11-03 18:36:22 +0100

    Setup project

---
+++ .mocharc.js
@@ -0,0 +1,12 @@
+const testPath = process.env.TEST_PATH || "tests/**/*.ts";
+
+module.exports = {
+  spec: [testPath],
+  require: ["ts-node/register", "./tests/mochaFixtures.ts"],
+  recursive: true,
+  extension: [
+    "ts"
+  ],
+  "parallel": true
+};
+
